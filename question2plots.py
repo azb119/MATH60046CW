2e4279769c3c36698bb2ff7f509dd8add9539b92
azb119 <azb119@ic.ac.uk>
2021-12-14 20:29:40 +0000

    2a

--- question2plots.py
+++ question2plots.py
@@ -15,11 +15,14 @@ f = np.linspace(-0.5, 0.5, len(spf)+1)[:-1] # to not incl 0.5
 
 plt.figure('periodogram')
 plt.plot(f, spf)
+plt.title('Periodogram')
+plt.xlabel('frequency')
+plt.ylabel('S(f)')
 
 plt.figure('direct spectral estimate')
 plt.plot(f,sdf)
 plt.title('Direct Spectral Estimate')
 plt.xlabel('frequency')
-plt.ylabel()
+plt.ylabel('S(f)')
 
 plt.show()
